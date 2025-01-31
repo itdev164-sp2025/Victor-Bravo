08f4b813ff6c90a2f78151ba57309316d3cfb8d4
Victor Bravo <bravovn@gmatc.matc.edu>
2025-01-30 19:58:26 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Blog`,
+    description: ``,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Victor Bravo`,
+      company: `Blogs Inc.`,
+      address: `PD Box 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
