f94d38ac667ec717ee008774cd318a52ce363a4c
Victor Bravo <bravovn@gmatc.matc.edu>
2025-04-03 20:15:44 -0500

    Completed Activity 7

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -5,6 +5,8 @@ import styled, {ThemeProvider} from "styled-components"
 import {Header} from "./Header"
 import "./layout.css"
 import {Gray} from "./themes/Gray"
+import {Main} from "./Main"
+import {Footer} from "./Footer"
 
 const Content = styled.div`
   margin: 0 auto;
@@ -12,10 +14,6 @@ const Content = styled.div`
   padding: var(--size-gutter);
 `
 
-const Footer = styled.footer`
-  margin-top: var(--space-5);
-  font-size: var(--font-sm);
-`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -32,7 +30,7 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={Gray}>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <Content>
-        <main>{children}</main>
+        <Main>{children}</Main>
         <Footer>
           © {new Date().getFullYear()} &middot; Built with
           {` `}
